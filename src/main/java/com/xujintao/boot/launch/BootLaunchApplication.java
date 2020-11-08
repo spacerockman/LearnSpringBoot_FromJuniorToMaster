2132ffef9b0955ba821dccd4c992d33663fee14d
EricXu <1306939732tao@gmail.com>
2020-11-09 01:04:09 +0900

    Initial commit

---
+++ src/main/java/com/xujintao/boot/launch/BootLaunchApplication.java
@@ -0,0 +1,13 @@
+package com.xujintao.boot.launch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLaunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLaunchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xujintao/boot/launch/BootLaunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xujintao.boot.launch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLaunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
